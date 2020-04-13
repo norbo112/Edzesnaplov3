6c392fa134c726a19c3b072e6f6c304744ccb7a3
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2020-04-13 16:07:31 +0200

    sorozatok sorbarendezésének tesztje

--- app/src/main/java/aa/droid/norbo/projects/edzesnaplo3/uiutils/lists/SorozatSorter.java
+++ app/src/main/java/aa/droid/norbo/projects/edzesnaplo3/uiutils/lists/SorozatSorter.java
@@ -12,20 +12,24 @@ import aa.droid.norbo.projects.edzesnaplo3.database.dao.SorozatWithGyakorlat;
 public class SorozatSorter {
     public static void rcGyakSorozatSort(List<NaploActivity.RCViewGyakSorozat> sorozats) {
         NaploActivity.RCViewGyakSorozat[] list = sorozats.toArray(new NaploActivity.RCViewGyakSorozat[0]);
-        System.out.println(Arrays.toString(list));
-        for (int i = 0; i < list.length-1; i++) {
-            if(isSorozatBefore(list[i], list[i+1]) > 0) {
-                NaploActivity.RCViewGyakSorozat tmp = list[i];
-                list[i] = list[i+1];
-                list[i+1] = tmp;
+        boolean csere;
+        int i = list.length - 1;
+        do {
+            csere = false;
+            for (int j = 0; j < i; j++) {
+                if(isSorozatBefore(list[j], list[j+1]) > 0) {
+                    NaploActivity.RCViewGyakSorozat tmp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1] = tmp;
+                    csere = true;
+                }
             }
-        }
+            i--;
+        } while (csere);
 
-        System.out.println("Rendezés után:");
-        System.out.println(Arrays.toString(list));
         sorozats.clear();
-        for (int i = 0; i < list.length; i++) {
-            sorozats.add(list[i]);
+        for (int k = 0; k < list.length; k++) {
+            sorozats.add(list[k]);
         }
     }
 
@@ -42,7 +46,7 @@ public class SorozatSorter {
             Scanner scanner = new Scanner(a.getSorozatList().get(a.getSorozatList().size() - 1).getIsmidopont());
             scanner.findWithinHorizon("(\\d+:)(\\d+:)(\\d+)", 0);
             MatchResult match = scanner.match();
-            scanner = new Scanner(b.getSorozatList().get(b.getSorozatList().size() - 1).getIsmidopont());
+            scanner = new Scanner(b.getSorozatList().get(0).getIsmidopont());
             scanner.findWithinHorizon("(\\d+:)(\\d+:)(\\d+)", 0);
             MatchResult match2 = scanner.match();
             a_time = new int[]{
@@ -53,9 +57,6 @@ public class SorozatSorter {
                     Integer.parseInt(match2.group(1).substring(0, match2.group(1).lastIndexOf(':'))),
                     Integer.parseInt(match2.group(2).substring(0, match2.group(1).lastIndexOf(':'))),
                     Integer.parseInt(match2.group(3))};
-            System.out.println();
-            System.out.println("A="+ Arrays.toString(a_time) +" B="+ Arrays.toString(b_time));
-            System.out.println();
             for (int i = 0; i < a_time.length; i++) {
                 if (a_time[i] == b_time[i]) continue;
                 if (a_time[i] > b_time[i]) return 1;
