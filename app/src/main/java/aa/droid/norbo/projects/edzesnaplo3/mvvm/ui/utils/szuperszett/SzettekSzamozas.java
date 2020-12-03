c0b6f1dcd5b920234b8435d27c529f02096eeb9d
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2020-12-03 12:20:45 +0100

    szettek felvétele, szuperszett tényleges tervezése, előkészítése

---
+++ app/src/main/java/aa/droid/norbo/projects/edzesnaplo3/mvvm/ui/utils/szuperszett/SzettekSzamozas.java
@@ -0,0 +1,57 @@
+package aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.szuperszett;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.scopes.ActivityScoped;
+
+@ActivityScoped
+public class SzettekSzamozas {
+    private static final Character SZUPERSZETT = 'S';
+    private static final Character TRISZETT = 'T';
+    private static final Character ORIASSZETT = 'O'; //négy gyakorlat egyszerre
+
+    @Inject
+    public SzettekSzamozas() {
+    }
+
+    public int getTriszettSzamozas(List<String> szettek) {
+        int n = 0;
+        int kovetkezoSzam;
+        for (int i = 0; i < szettek.size(); i++) {
+            String s = szettek.get(i);
+            if(s.charAt(0) == TRISZETT) {
+                n++;
+            }
+        }
+        kovetkezoSzam = n / 3 + 1;
+        return kovetkezoSzam;
+    }
+
+    public int getSzuperszettSzamozas(List<String> szettek) {
+        int n = 0;
+        int kovetkezoSzam;
+        for (int i = 0; i < szettek.size(); i++) {
+            String s = szettek.get(i);
+            if(s.charAt(0) == SZUPERSZETT) {
+                n++;
+            }
+        }
+        kovetkezoSzam = n / 2 + 1;
+        return kovetkezoSzam;
+    }
+
+    public int getOriasSzamozas(List<String> szettek) {
+        int n = 0;
+        int kovetkezoSzam;
+        for (int i = 0; i < szettek.size(); i++) {
+            String s = szettek.get(i);
+            if(s.charAt(0) == ORIASSZETT) {
+                n++;
+            }
+        }
+        kovetkezoSzam = n / 4 + 1;
+        return kovetkezoSzam;
+    }
+}
