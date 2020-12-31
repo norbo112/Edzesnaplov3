047e208d51edc4ba66253d342f5e12327b214bee
Iglói Norbert <37663391+norbo112@users.noreply.github.com>
2020-12-31 23:43:32 +0100

    szettek - javítások - clear

--- app/src/main/java/aa/droid/norbo/projects/edzesnaplo3/mvvm/ui/fortabs/TevekenysegFragment.java
+++ app/src/main/java/aa/droid/norbo/projects/edzesnaplo3/mvvm/ui/fortabs/TevekenysegFragment.java
@@ -33,7 +33,6 @@ import aa.droid.norbo.projects.edzesnaplo3.mvvm.db.entities.Sorozat;
 import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.fortabs.adatkozlo.AdatKozloInterface;
 import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.DateTimeFormatter;
 import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.ModelConverter;
-import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.debug.info.LayoutInfo;
 import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.naplo.NaploWorker;
 import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.naplo.SorozatUtil;
 import aa.droid.norbo.projects.edzesnaplo3.mvvm.ui.utils.szuperszett.SzettekSzamozas;
